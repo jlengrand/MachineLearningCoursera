50710f0ce21eb4fc2283761b8e22df07b5603312
julien Lengrand-Lambert <julien@lengrand.fr>
2015-12-24 14:10:25 +0100

    Answers two first questions of PCA exercise.

--- machine-learning-ex7/ex7/pca.m
+++ machine-learning-ex7/ex7/pca.m
@@ -1,4 +1,4 @@
-function [U, S] = pca(X)
+function [U, S, X] = pca(X)
 %PCA Run principal component analysis on the dataset X
 %   [U, S, X] = pca(X) computes eigenvectors of the covariance matrix of X
 %   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
@@ -20,11 +20,9 @@ S = zeros(n);
 %       number of examples).
 %
 
+sigma = 1/m * ( X'*X);
 
-
-
-
-
+[U, S, V] = svd(sigma);
 
 % =========================================================================
 
